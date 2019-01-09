f19634d6e0b259a4f81caa07baf8148ace869b28
t0mat0sk <t0m0.piii.12.07sk@gmail.com>
2019-01-09 22:08:29 +0900

    first commit

---
+++ app/src/androidTest/java/musicplayer/t0m0piii/com/musicplayer/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package musicplayer.t0m0piii.com.musicplayer
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("musicplayer.t0m0piii.com.musicplayer", appContext.packageName)
+    }
+}
