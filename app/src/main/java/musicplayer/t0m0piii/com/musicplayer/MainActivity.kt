57590b620f5e197cc25d29708cf0b5e1f27c54f8
t0mat0sk <t0m0.piii.12.07sk@gmail.com>
2019-01-10 21:27:51 +0900

    音を再生できるようにした

--- app/src/main/java/musicplayer/t0m0piii/com/musicplayer/MainActivity.kt
+++ app/src/main/java/musicplayer/t0m0piii/com/musicplayer/MainActivity.kt
@@ -1,8 +1,11 @@
 package musicplayer.t0m0piii.com.musicplayer
 
+import android.database.Cursor
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.loader.content.CursorLoader
 import com.google.android.exoplayer2.DefaultLoadControl
 import com.google.android.exoplayer2.DefaultRenderersFactory
 import com.google.android.exoplayer2.ExoPlayerFactory
@@ -12,6 +15,7 @@ import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +23,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var player: SimpleExoPlayer
     private lateinit var mediaSource: MediaSource
     private lateinit var extractorMediaSourceFactory: ExtractorMediaSource.Factory
+    private lateinit var cursor: Cursor
+    private var pathIndex: Int = 0
+    private var nameIndex: Int = 0
     private val playerView by lazy {
         findViewById<PlayerView>(R.id.playerView)
     }
@@ -27,7 +34,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        uri = Uri.parse("hogehoge")
+        val selection = MediaStore.Files.FileColumns.MEDIA_TYPE + "=" + MediaStore.Files.FileColumns.MEDIA_TYPE_AUDIO
+        val  projection: Array<String> = arrayOf(MediaStore.Images.Media.DATA, MediaStore.Images.Media.DISPLAY_NAME)
+        val queryUri = MediaStore.Files.getContentUri("external")
+        val cursorLoader = CursorLoader(
+            applicationContext,
+            queryUri,
+            projection,
+            selection,
+            null,
+            MediaStore.Files.FileColumns.DATE_ADDED + " DESC")
+
+        cursor = cursorLoader.loadInBackground()!!
+        pathIndex = cursor.getColumnIndex(MediaStore.Images.Media.DATA)
+        nameIndex = cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME)
+        cursor.moveToFirst()
+
+        uri = Uri.fromFile(File(cursor.getString(pathIndex)))
         player = ExoPlayerFactory.newSimpleInstance(
             DefaultRenderersFactory(applicationContext),
             DefaultTrackSelector(),
