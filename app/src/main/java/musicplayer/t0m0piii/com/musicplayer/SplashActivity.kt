c89d1e02f35ad9157d24461287ca65985b676d2a
t0mat0sk <t0m0.piii.12.07sk@gmail.com>
2019-01-13 15:30:06 +0900

    splash画面実装

---
+++ app/src/main/java/musicplayer/t0m0piii/com/musicplayer/SplashActivity.kt
@@ -0,0 +1,40 @@
+package musicplayer.t0m0piii.com.musicplayer
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    val REQUEST_READ_EXTERNAL_STORAGE = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE)
+            } else {
+                moveToMusicActivity()
+            }
+        }, 1000)
+    }
+
+    fun moveToMusicActivity() {
+        val intent = Intent(this, MusicActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_READ_EXTERNAL_STORAGE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                moveToMusicActivity()
+            }
+        }
+    }
+}
